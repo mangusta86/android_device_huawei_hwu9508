5a9b7de2f0834cb12a6c95b954f82a79c6fac877
mangusta86 <fassimarco86@gmail.com>
2014-01-28 18:54:05 +0100

    fix ramdisk compiling issues

--- full_hwu9508.mk
+++ full_hwu9508.mk
@@ -39,20 +39,23 @@ PRODUCT_AAPT_CONFIG := normal mdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 LOCAL_PATH := device/huawei/hwu9508
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
+
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_KERNEL):kernel
 
 # init files from Huawei initramfs
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.rc:root/init.rc \
     $(LOCAL_PATH)/init.trace.rc:root/init.trace.rc \
     $(LOCAL_PATH)/init.k3v2oem1.rc:root/init.k3v2oem1.rc \
     $(LOCAL_PATH)/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_PATH)/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/ueventd.k3v2oem1.rc:root/ueventd.k3v2oem1.rc
 
 # Install the features available on this device.

bb5ae2431e286605c10d5112cf1cc79220d5b586
Marco Fassi <marco@marco-Aspire-5742G.(none)>
2014-03-22 15:10:45 +0000

    add iherit from full telephony in device file

--- device_u9508.mk
+++ device_u9508.mk
@@ -1,6 +1,8 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
